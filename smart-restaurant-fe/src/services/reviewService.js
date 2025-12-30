827118819d3ef4a200c59a0a7aff9af3ec071960
Otis <ledat02072005@gmail.com>
2025-12-30 23:58:10 +0700

    refactor: Update reviewService to support restaurantId parameter
    - Update getReviews() to accept restaurantId as first parameter
    - Change API endpoint from /reviews/{menuItemId} to /reviews/{restaurantId}/{menuItemId}
    - Add documentation comment for updateReview id parameter
    - Align service with backend route structure

--- smart-restaurant-fe/src/services/reviewService.js
+++ smart-restaurant-fe/src/services/reviewService.js
@@ -1,14 +1,15 @@
 import api from "./api";
 
 export const reviewService = {
-    getReviews: async (menuItemId) => {
-        const response = await api.get(`/reviews/${menuItemId}`);
+    getReviews: async (restaurantId, menuItemId) => {
+        const response = await api.get(`/reviews/${restaurantId}/${menuItemId}`);
         return response.data;
     },
     addReview: async (data) => {
         const response = await api.post("/reviews", data);
         return response.data;
     },
+    //id ở đây là id của review cần sửa
     updateReview: async (id, data) => {
         const response = await api.put(`/reviews/${id}`, data);
         return response.data;
