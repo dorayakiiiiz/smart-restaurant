8fac652f14b959e69d012aba637d82252331a410
dorayakiiiiz <travansy2305@gmail.com>
2026-01-05 00:02:55 +0700

    fix bug socket deploy

--- smart-restaurant-fe/src/services/socket.js
+++ smart-restaurant-fe/src/services/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // URL Backend
-const SOCKET_URL = import.meta.env.VITE_API_URL?.replace('/api', '') || "http://localhost:5000";
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
 export const socket = io(SOCKET_URL, {
     withCredentials: true,
