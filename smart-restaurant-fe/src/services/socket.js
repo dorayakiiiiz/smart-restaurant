c5be9c339d0e3571153cbb90e0e1aace69ee0c3f
dorayakiiiiz <travansy2305@gmail.com>
2026-01-05 19:45:33 +0700

    fixing payos webhook

--- smart-restaurant-fe/src/services/socket.js
+++ smart-restaurant-fe/src/services/socket.js
@@ -5,5 +5,6 @@ const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
 export const socket = io(SOCKET_URL, {
     withCredentials: true,
-    autoConnect: false // Chỉ connect khi cần thiết
+    autoConnect: false, // Chỉ connect khi cần thiết,
+    transports: ['websocket']
 });
