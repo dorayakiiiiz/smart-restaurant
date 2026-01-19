7155012fc0508c89734d7ae2c02d38ca67824c48
dorayakiiiiz <travansy2305@gmail.com>
2026-01-20 00:13:58 +0700

    commit

--- smart-restaurant-fe/src/layouts/AdminDashboardLayout.jsx
+++ smart-restaurant-fe/src/layouts/AdminDashboardLayout.jsx
@@ -44,7 +44,7 @@ export default function AdminDashboardLayout() {
         <div className="w-full h-screen flex flex-col font-quicksand font-medium bg-[#f8f9fa]">
             {/* Header */}
             <div className="bg-[#1a1a1a] h-[70px] w-full flex items-center justify-between px-6 shadow-md z-20">
-                <Link to="/system/admin/dashboard" className="text-[#D4AF37] font-momo text-2xl font-bold flex items-center gap-2">
+                <Link to="/" className="text-[#D4AF37] font-momo text-2xl font-bold flex items-center gap-2">
                     <i className="fa-solid fa-utensils text-yellow-700"></i>
                     Smart Restaurant 
                     <span className="text-[10px] tracking-wider text-white font-bold border border-gray-600 px-2 py-0.5 rounded ml-2 uppercase bg-gray-800">
